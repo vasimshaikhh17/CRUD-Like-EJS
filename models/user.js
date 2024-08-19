5d6c636819a5599d731e2cf247de52c0800d59bb
vasimshaikhh17@gmail.com <alim@whitesharkindia.com>
2024-08-19 14:47:28 +0530

    finished

---
+++ models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://rehbargrowwindia:pJQYJqsUtVSuLsnj@learning.xkvhh.mongodb.net/miniproject")
+
+
+const userSchema = mongoose.Schema({
+    username:String,
+    name:String,
+    age:String,
+    email:String,
+    password:String,
+    posts:[
+        {type:mongoose.Schema.ObjectId, ref:"post"}
+    ]
+})
+
+module.exports = mongoose.model('user',userSchema);
